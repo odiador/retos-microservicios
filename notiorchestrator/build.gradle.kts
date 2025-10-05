414ff45072cfc24e548b58531601bff599232e1e
Ana María <anicu2314@gmail.com>
2025-10-05 14:36:45 -0500

    Cambios en el .feature del sms

--- notiorchestrator/build.gradle.kts
+++ notiorchestrator/build.gradle.kts
@@ -25,6 +25,16 @@ dependencies {
     testImplementation("org.springframework.amqp:spring-rabbit-test")
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation 'io.rest-assured:rest-assured:5.4.0'
+    testImplementation 'io.rest-assured:json-path:5.4.0'
+    testImplementation 'io.rest-assured:xml-path:5.4.0'
+
+    testImplementation 'io.cucumber:cucumber-java:7.18.0'
+    testImplementation 'io.cucumber:cucumber-spring:7.18.0'
+    testImplementation 'io.cucumber:cucumber-junit-platform-engine:7.18.0'
+
+    testImplementation 'org.junit.platform:junit-platform-suite:1.10.2'
 }
 
 java {
